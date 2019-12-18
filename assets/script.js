29fae53f931442da641afcf3541a8b00d50e624a
Red Skull <bradfordmichael7@gmail.com>
2019-12-18 13:35:14 -0500

    added initial js and uploading to attempt to solve cors issue

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    let queryURL = 'api.openweathermap.org/data/2.5/weather?q=';
+    let APIkey = '&APPID=83b81619b1ba1aed2a20cc81447476e3';
+    let city;
+
+    $('#search').on('click', function() {
+        event.preventDefault();
+        city = $('#input').val().trim();
+        console.log(city);
+        $.ajax({
+            url: queryURL + city + APIkey,
+            method: 'GET'
+        }).then(
+            function(response) {
+                console.log(response);
+            }
+        )
+    })
+})
