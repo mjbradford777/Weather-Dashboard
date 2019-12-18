5948a6f25417298149597f2288eb56719d8acba5
Red Skull <bradfordmichael7@gmail.com>
2019-12-18 15:21:23 -0500

    added functionality to call api for all relevant weather info and console log results

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,56 @@
 $(document).ready(function() {
-    let queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=';
+    let cityQueryURL = 'https://api.openweathermap.org/data/2.5/weather?q=';
+    let uvQueryURL = 'http://api.openweathermap.org/data/2.5/uvi?appid=83b81619b1ba1aed2a20cc81447476e3&lat=';
+    let forecastQueryURL = 'https://api.openweathermap.org/data/2.5/forecast?q={city name},{country code}';
     let APIkey = '&appid=83b81619b1ba1aed2a20cc81447476e3';
     let city;
+    let longitude;
+    let latitude;
+    let code;
 
     $('#search').on('click', function() {
         event.preventDefault();
         city = $('#input').val().trim();
         console.log(city);
         $.ajax({
-            url: queryURL + city + APIkey,
+            url: cityQueryURL + city + APIkey,
+            method: 'GET'
+        }).then(
+            function(response) {
+                console.log(response);
+                console.log(response.name);
+                console.log(moment().format('MMMM Do YYYY'));
+                console.log(response.main.temp);
+                console.log(response.main.humidity);
+                console.log(response.wind.speed);
+                console.log(response.coord.lon);
+                console.log(typeof(response.coord.lon));
+                longitude = response.coord.lon;
+                console.log(response.coord.lat);
+                console.log(typeof(response.coord.lat));
+                latitude = response.coord.lat;
+                console.log(response.sys.country);
+                console.log(typeof(response.sys.country));
+                code = response.sys.country;
+            }
+        );
+
+        $.ajax({
+            url: uvQueryURL + latitude + '&lon=' + longitude,
             method: 'GET'
         }).then(
             function(response) {
                 console.log(response);
             }
-        )
+        );
 
-        // $.ajax({
-        //     url: 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=83b81619b1ba1aed2a20cc81447476e3',
-        //     method: 'GET'
-        // }).then(
-        //     function(response) {
-        //         console.log(response);
-        //     }
-        // )
+        $.ajax({
+            url: forecastQueryURL + city + ',' + code + APIkey,
+            method: 'GET'
+        }).then(
+            function(response) {
+                console.log(response);
+            }
+        );
     })
 })
