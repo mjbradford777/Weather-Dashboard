32abfe68c44a46d473908884b12d2f1d2def70c3
Red Skull <bradfordmichael7@gmail.com>
2019-12-18 13:49:25 -0500

    trying api call fix

--- assets/script.js
+++ assets/script.js
@@ -7,8 +7,17 @@ $(document).ready(function() {
         event.preventDefault();
         city = $('#input').val().trim();
         console.log(city);
+        // $.ajax({
+        //     url: queryURL + city + APIkey,
+        //     method: 'GET'
+        // }).then(
+        //     function(response) {
+        //         console.log(response);
+        //     }
+        // )
+
         $.ajax({
-            url: queryURL + city + APIkey,
+            url: 'api.openweathermap.org/data/2.5/weather?q=London&APPID=83b81619b1ba1aed2a20cc81447476e3',
             method: 'GET'
         }).then(
             function(response) {
